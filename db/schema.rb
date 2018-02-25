02cf93431f683d51caf64258931f1050bab8d81a
tomoyukikashiro <ksrtmyk@gmail.com>
2018-02-25 15:02:08 +0900

    feat(model): added single table inheritance

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180225052828) do
+ActiveRecord::Schema.define(version: 20180225055110) do
 
   create_table "employees", force: :cascade do |t|
     t.string "name"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 20180225052828) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "vehicles", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
